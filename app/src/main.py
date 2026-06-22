67f8bf1766b52f99b0a4b7708e16eb7fc592b9d1
senai_25_26 <guilhermemaurinhoborgesalves@gmail.com>
2026-06-22 11:07:24 -0300

    APP

--- app/src/main.py
+++ app/src/main.py
@@ -1,8 +1,9 @@
 import asyncio
-
 import flet as ft
 from flet import Colors, View, CrossAxisAlignment, ListView, Card, Container, Text
 from rotas import rastrear_encomenda
+from datetime import datetime
+
 
 def main(page: ft.Page):
     page.title = "App FlashLog"
@@ -13,38 +14,158 @@ def main(page: ft.Page):
     page.window.width = 373
     page.window.height = 695
 
-    list_view = ListView(width=373, height=250)
-
-    input_code = ft.TextField(
-        label=Text("Digite o Codigo de Rastreio", color=Colors.WHITE),
-        prefix_icon=ft.Icon(ft.Icons.SEARCH, color=Colors.WHITE),
-        color=Colors.WHITE,
-        on_submit=lambda _: pesquisar_encomenda(),
-    )
-
     def navegar(route):
         asyncio.create_task(
             page.push_route(route)
         )
 
+    def definir_icones(status):
+        if status == "POSTADO":
+            return ft.Icons.OUTBOX_OUTLINED
+        elif status == "TRANSITO":
+            return ft.Icons.LOCAL_SHIPPING_OUTLINED
+        elif status == "CAMINHO":
+            return ft.Icons.ROUTE
+        elif status == "ENTREGUE":
+            return ft.Icons.CHECK_CIRCLE_OUTLINE
+
+    def definir_data(status,data):
+        data_ = datetime.strptime(data, "%a, %d %b %Y %H:%M:%S GMT")
+
+        result_data = data_.strftime("%d/%m/%Y - %H:%M")
+
+        if status == "POSTADO":
+            return result_data
+        elif status == "TRANSITO":
+            return result_data
+        elif status == "CAMINHO":
+            return result_data
+        elif status == "ENTREGUE":
+            return result_data
+
+    def definir_msg(status):
+        if status == "POSTADO":
+            return f"Encomenda Postada"
+        elif status == "TRANSITO":
+            return f"Encomenda em Transito"
+        elif status == "CAMINHO":
+            return f"Encomenda a Caminho da sua Casa"
+        elif status == "ENTREGUE":
+            return f"Encomenda Entregue"
+
+    def definir_msg2(status, galpao):
+        if status == "ENTRADA":
+            return f"Encomenda chegou em {galpao}"
+        elif status == "SAIDA":
+            return f"Encomenda saiu de {galpao}"
+
     def montar_lista_encomenda():
         list_view.controls.clear()
-        for i in rastrear_encomenda(input_code.value):
-            list_view.controls.append(
+
+        codigo = rastrear_encomenda(input_code.value)
+
+        if len(codigo) == 0:
+            return list_view.controls.append(
                 ft.Card(
-                    bgcolor="#0A4682",
-                    content = ft.Container(
+                    bgcolor="#FD8065",
+                    content=ft.Container(
                         ft.Row(
-                            margin=ft.Margin.all(5),
+                            margin=ft.Margin.all(8),
                             spacing=12,
                             controls=[
+                                ft.Container(
+                                    width=35,
+                                    height=35,
+                                    bgcolor="#FF4F28",
+                                    border_radius=25,
+                                    alignment=ft.Alignment.CENTER,
+                                    content=ft.Icon(ft.Icons.CLOSE)
+                                ),
+                                ft.Column(
+                                    [
+                                        ft.Text(
+                                            'Nenhuma Encomenda Encontrada'
 
+                                        ),
+                                    ]
+                                )
+                            ]
+                        ),
+                    )
+                )
+            )
+
+        if input_code.value == "" or input_code.value == None:
+            return list_view.controls.append(
+                ft.Card(
+                    bgcolor="#FD8065",
+                    content=ft.Container(
+                        ft.Row(
+                            margin=ft.Margin.all(8),
+                            spacing=12,
+                            controls=[
+                                ft.Container(
+                                    width=35,
+                                    height=35,
+                                    bgcolor="#FF4F28",
+                                    border_radius=25,
+                                    alignment=ft.Alignment.CENTER,
+                                    content=ft.Icon(ft.Icons.CLOSE)
+                                ),
+                                ft.Column(
+                                    [
+                                        ft.Text(
+                                            'Nenhuma Encomenda Encontrada'
+
+                                        ),
+                                    ]
+                                )
+                            ]
+                        ),
+                    )
+                )
+            )
+
+        for i in codigo:
+            list_view.controls.append(
+                ft.Card(
+                    bgcolor="#FD8065",
+                    content=ft.Container(
+                        ft.Row(
+                            margin=ft.Margin.all(8),
+                            spacing=12,
+                            controls=[
+                                ft.Container(
+                                    width=35,
+                                    height=35,
+                                    bgcolor="#FF4F28",
+                                    border_radius=25,
+                                    alignment=ft.Alignment.CENTER,
+                                    content=ft.Icon(definir_icones(i['encomenda']['status_encomenda']))
+                                ),
+                                ft.Column(
+                                    [
+                                        ft.Text(
+                                            definir_data(i['encomenda']['status_encomenda'],i['movimentacao']['data_atual']),
+                                        ),
+                                        ft.Text(
+                                            definir_msg(i['encomenda']['status_encomenda']),
+                                            weight=ft.FontWeight.BOLD,
+                                        ),
+                                        ft.Text(
+                                            definir_msg2(i['movimentacao']['status_movimentacao'],
+                                                         f"{i['galpao']['cidade']}/{i['galpao']['estado']}"),
+                                            size=10,
+                                        )
+                                    ]
+                                )
                             ]
                         )
                     )
-
                 )
             )
+
+            page.update()
 
 
     def route_change(e=None):
@@ -125,7 +246,7 @@ def main(page: ft.Page):
                                         ft.Container(height=20),
                                         ft.Button(
                                             ft.Text("ENTRAR", size=22, weight=ft.FontWeight.BOLD),
-                                            on_click=lambda _: navegar("/segunda_tela"),
+                                            on_click=lambda _: navegar("/tela_pesquisa"),
                                             margin=ft.Margin.only(bottom=180, left=9),
                                             bgcolor="#17385B",
                                             color=ft.Colors.WHITE,
@@ -143,11 +264,10 @@ def main(page: ft.Page):
                 )
             )
 
-        elif page.route == "/segunda_tela":
-            montar_lista_encomenda()
+        elif page.route == "/tela_pesquisa":
             page.views.append(
                 View(
-                    route="/segunda_tela",
+                    route="/tela_pesquisa",
                     padding=0,
                     controls=[
                         ft.Container(
@@ -175,7 +295,6 @@ def main(page: ft.Page):
                 )
             )
 
-            page.update()
 
     async def view_pop(e):
         if e.view is not None:
@@ -184,13 +303,19 @@ def main(page: ft.Page):
             await page.push_route(top_view.route)
 
 
+    input_code = ft.TextField(
+        label=Text("Digite o Codigo de Rastreio", color=Colors.WHITE),
+        prefix_icon=ft.Icon(ft.Icons.SEARCH, color=Colors.WHITE),
+        color=Colors.WHITE,
+        on_submit=lambda _: montar_lista_encomenda(),
+    )
+
+    list_view = ListView(width=373, height=450)
+
     page.on_route_change = route_change
     page.on_view_pop = view_pop
 
     page.update()
     route_change()
-
-
-
 
 ft.run(main)
--- app/src/rotas.py
+++ app/src/rotas.py
@@ -1,6 +1,6 @@
 import requests
 
-base_url = "http://10.135.232.36:5006"
+base_url = "http://10.135.232.19:5006"
 
 def rastrear_encomenda(codigo):
     url = f"{base_url}/pesquisar_encomenda"
